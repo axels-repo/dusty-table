fe0a81d44c534447a7e1932608c8b0d9043893d5
Axel-12246765 <axelbaylon@hotmail.com>
2020-08-26 18:43:56 +1000

    Added test for Calc class subtract method

--- src/test/java/CalcTest.java
+++ src/test/java/CalcTest.java
@@ -5,9 +5,15 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CalcTest {
 
+
     @Test
     public void testAdd() {
-        int expectedResult = 10;
-        assertEquals(expectedResult, Calc.add(8,2));
+        int expectedAddResult = 10;
+        assertEquals(expectedAddResult, Calc.add(8,2));
+    }
+
+    public void testSubtract() {
+        int expectedSubtractResult = 6;
+        assertEquals(expectedSubtractResult, Calc.subtract(8,2));
     }
 }
